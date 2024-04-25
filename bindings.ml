f9cf5becc419b4b51a3e67fa0cc3c868e84871ff
Etienne Millon <me@emillon.org>
2024-04-25 14:09:07 +0200

    no C

--- bindings.ml
+++ bindings.ml
@@ -7,6 +7,15 @@ module Types (X : Ctypes.TYPE) = struct
   let handled_access_fs = X.field ruleset_attr "handled_access_fs" X.int
   let handled_access_net = X.field ruleset_attr "handled_access_net" X.int
   let () = X.seal ruleset_attr
+
+  type path_beneath_attr
+
+  let path_beneath_attr : path_beneath_attr Ctypes.structure X.typ =
+    X.structure "landlock_path_beneath_attr"
+
+  let allowed_access = X.field path_beneath_attr "allowed_access" X.int
+  let parent_fd = X.field path_beneath_attr "parent_fd" X.int
+  let () = X.seal path_beneath_attr
   let landlock_access_fs_execute = X.constant "LANDLOCK_ACCESS_FS_EXECUTE" X.int
 
   let landlock_access_fs_write_file =
@@ -60,11 +69,15 @@ module Types (X : Ctypes.TYPE) = struct
     X.constant "SYS_landlock_create_ruleset" X.int
 
   let sys_landlock_restrict_self = X.constant "SYS_landlock_restrict_self" X.int
+  let sys_landlock_add_rule = X.constant "SYS_landlock_add_rule" X.int
 
   let landlock_create_ruleset_version =
     X.constant "LANDLOCK_CREATE_RULESET_VERSION" X.uint32_t
 
+  let landlock_rule_path_beneath = X.constant "LANDLOCK_RULE_PATH_BENEATH" X.int
   let pr_set_no_new_privs = X.constant "PR_SET_NO_NEW_PRIVS" X.int
+  let o_path = X.constant "O_PATH" X.int
+  let o_cloexec = X.constant "O_CLOEXEC" X.int
 end
 
 module Functions (X : Ctypes.FOREIGN) = struct
@@ -75,7 +88,13 @@ module Functions (X : Ctypes.FOREIGN) = struct
   let landlock_restrict_self =
     X.foreign "syscall" Ctypes.(X.(int @-> int @-> int @-> returning int))
 
+  let landlock_add_rule =
+    X.foreign "syscall"
+      Ctypes.(X.(int @-> int @-> int @-> ptr void @-> int @-> returning int))
+
   let prctl =
     X.foreign "prctl"
       Ctypes.(X.(int @-> int @-> int @-> int @-> int @-> returning int))
+
+  let open_ = X.foreign "open" Ctypes.(X.(string @-> int @-> returning int))
 end
--- landlock.ml
+++ landlock.ml
@@ -1,62 +1,66 @@
+let list_to_int to_int l = List.fold_left (fun acc a -> acc lor to_int a) 0 l
+let int_to_fd : int -> Unix.file_descr = Obj.magic
+let fd_to_int : Unix.file_descr -> int = Obj.magic
+
+module Access_fs = struct
+  type t =
+    | Execute
+    | Write_file
+    | Read_file
+    | Read_dir
+    | Remove_dir
+    | Remove_file
+    | Make_char
+    | Make_dir
+    | Make_reg
+    | Make_sock
+    | Make_fifo
+    | Make_block
+    | Make_sym
+    | Refer
+    | Truncate
+
+  let to_int =
+    let open C.Types in
+    function
+    | Execute -> landlock_access_fs_execute
+    | Write_file -> landlock_access_fs_write_file
+    | Read_file -> landlock_access_fs_read_file
+    | Read_dir -> landlock_access_fs_read_dir
+    | Remove_dir -> landlock_access_fs_remove_dir
+    | Remove_file -> landlock_access_fs_remove_file
+    | Make_char -> landlock_access_fs_make_char
+    | Make_dir -> landlock_access_fs_make_dir
+    | Make_reg -> landlock_access_fs_make_reg
+    | Make_sock -> landlock_access_fs_make_sock
+    | Make_fifo -> landlock_access_fs_make_fifo
+    | Make_block -> landlock_access_fs_make_block
+    | Make_sym -> landlock_access_fs_make_sym
+    | Refer -> landlock_access_fs_refer
+    | Truncate -> landlock_access_fs_truncate
+
+  let supported_in ~abi = function
+    | Refer -> abi >= 2
+    | Truncate -> abi >= 3
+    | Execute | Write_file | Read_file | Read_dir | Remove_dir | Remove_file
+    | Make_char | Make_dir | Make_reg | Make_sock | Make_fifo | Make_block
+    | Make_sym ->
+        true
+end
+
+module Access_net = struct
+  type t = Bind_tcp | Connect_tcp
+
+  let to_int =
+    let open C.Types in
+    function
+    | Bind_tcp -> landlock_access_net_bind_tcp
+    | Connect_tcp -> landlock_access_net_connect_tcp
+
+  let supported_in ~abi = function Bind_tcp | Connect_tcp -> abi >= 4
+end
+
 module Ruleset_attr = struct
-  module Access_fs = struct
-    type t =
-      | Execute
-      | Write_file
-      | Read_file
-      | Read_dir
-      | Remove_dir
-      | Remove_file
-      | Make_char
-      | Make_dir
-      | Make_reg
-      | Make_sock
-      | Make_fifo
-      | Make_block
-      | Make_sym
-      | Refer
-      | Truncate
-
-    let to_int =
-      let open C.Types in
-      function
-      | Execute -> landlock_access_fs_execute
-      | Write_file -> landlock_access_fs_write_file
-      | Read_file -> landlock_access_fs_read_file
-      | Read_dir -> landlock_access_fs_read_dir
-      | Remove_dir -> landlock_access_fs_remove_dir
-      | Remove_file -> landlock_access_fs_remove_file
-      | Make_char -> landlock_access_fs_make_char
-      | Make_dir -> landlock_access_fs_make_dir
-      | Make_reg -> landlock_access_fs_make_reg
-      | Make_sock -> landlock_access_fs_make_sock
-      | Make_fifo -> landlock_access_fs_make_fifo
-      | Make_block -> landlock_access_fs_make_block
-      | Make_sym -> landlock_access_fs_make_sym
-      | Refer -> landlock_access_fs_refer
-      | Truncate -> landlock_access_fs_truncate
-
-    let supported_in ~abi = function
-      | Refer -> abi >= 2
-      | Truncate -> abi >= 3
-      | Execute | Write_file | Read_file | Read_dir | Remove_dir | Remove_file
-      | Make_char | Make_dir | Make_reg | Make_sock | Make_fifo | Make_block
-      | Make_sym ->
-          true
-  end
-
-  module Access_net = struct
-    type t = Bind_tcp | Connect_tcp
-
-    let to_int =
-      let open C.Types in
-      function
-      | Bind_tcp -> landlock_access_net_bind_tcp
-      | Connect_tcp -> landlock_access_net_connect_tcp
-
-    let supported_in ~abi = function Bind_tcp | Connect_tcp -> abi >= 4
-  end
-
   type t = {
     handled_access_fs : Access_fs.t list;
     handled_access_net : Access_net.t list;
@@ -70,8 +74,6 @@ module Ruleset_attr = struct
         List.filter (Access_net.supported_in ~abi) r.handled_access_net;
     }
 
-  let list_to_int to_int l = List.fold_left (fun acc a -> acc lor to_int a) 0 l
-
   let as_ptr { handled_access_fs; handled_access_net } =
     let open Ctypes in
     let p = allocate_n C.Types.ruleset_attr ~count:1 in
@@ -82,16 +84,23 @@ module Ruleset_attr = struct
     p
 end
 
-external setup_landlock_ : Unix.file_descr -> unit = "setup_landlock"
+module Path_beneath_attr = struct
+  type t = { allowed_access : Access_fs.t list; parent_fd : Unix.file_descr }
+
+  let as_ptr { allowed_access; parent_fd } =
+    let open Ctypes in
+    let p = allocate_n C.Types.path_beneath_attr ~count:1 in
+    p |-> C.Types.allowed_access <-@ list_to_int Access_fs.to_int allowed_access;
+    p |-> C.Types.parent_fd <-@ fd_to_int parent_fd;
+    p
+end
 
 let get_abi () =
   C.Functions.landlock_create_ruleset C.Types.sys_landlock_create_ruleset
     Ctypes.null Unsigned.Size_t.zero C.Types.landlock_create_ruleset_version
 
-let int_to_fd : int -> Unix.file_descr = Obj.magic
-let fd_to_int : Unix.file_descr -> int = Obj.magic
-
-let create_ruleset p_ruleset_attr =
+let create_ruleset ruleset_attr =
+  let p_ruleset_attr = ruleset_attr |> Ruleset_attr.as_ptr |> Ctypes.to_voidp in
   C.Functions.landlock_create_ruleset C.Types.sys_landlock_create_ruleset
     p_ruleset_attr
     (Unsigned.Size_t.of_int (Ctypes.sizeof C.Types.ruleset_attr))
@@ -108,6 +117,32 @@ let restrict_self ruleset_fd =
 let no_new_privs () =
   let err = C.Functions.prctl C.Types.pr_set_no_new_privs 1 0 0 0 in
   if err <> 0 then failwith "prctl"
+
+let open_dir path =
+  let fd = C.Functions.open_ path C.Types.(o_path lor o_cloexec) in
+  if fd < 0 then failwith "open_dir";
+  int_to_fd fd
+
+let add_rule ruleset_fd path_beneath =
+  let p_path_beneath =
+    Path_beneath_attr.as_ptr path_beneath |> Ctypes.to_voidp
+  in
+  let err =
+    C.Functions.landlock_add_rule C.Types.sys_landlock_add_rule
+      (fd_to_int ruleset_fd) C.Types.landlock_rule_path_beneath p_path_beneath 0
+  in
+  if err <> 0 then failwith "landlock_add_rule"
+
+let with_fd f fd =
+  Fun.protect (fun () -> f fd) ~finally:(fun () -> Unix.close fd)
+
+let with_open_dir path f =
+  let fd = open_dir path in
+  with_fd f fd
+
+let with_ruleset attrs f =
+  let fd = create_ruleset attrs in
+  with_fd f fd
 
 let setup_landlock () =
   let ruleset_attr : Ruleset_attr.t =
@@ -134,14 +169,14 @@ let setup_landlock () =
     }
   in
   let ruleset_attr = Ruleset_attr.filter ruleset_attr ~abi:(get_abi ()) in
-  let p_ruleset_attr = Ruleset_attr.as_ptr ruleset_attr in
-  let ruleset_fd = create_ruleset (Ctypes.to_voidp p_ruleset_attr) in
-  Fun.protect
-    (fun () ->
-      setup_landlock_ ruleset_fd;
+  with_ruleset ruleset_attr (fun ruleset_fd ->
+      with_open_dir "/usr" (fun parent_fd ->
+          let path_beneath : Path_beneath_attr.t =
+            { allowed_access = [ Execute; Read_file; Read_dir ]; parent_fd }
+          in
+          add_rule ruleset_fd path_beneath);
       no_new_privs ();
       restrict_self ruleset_fd)
-    ~finally:(fun () -> Unix.close ruleset_fd)
 
 let can_read path =
   let ok = ref false in
